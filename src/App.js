0016f53a35924fabd8b3d4d961a5334338b4f16e
Kelly McCormack <contact@kellymccormack.com>
2024-07-28 13:59:46 -0500

    Initial commit - based on diceroller example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  // let result;
+  // new state vars
+  // const [image, setNewImage] = useState(diceImages[0]);
+  // const [image2, setNewImage2] = useState(diceImages[1]);
+  // "need to call the useState hook and pass in the initial value you want to assign to that variable"
+  const [result, getResult] = useState(0);
+
+  const [diceHand, getDicehand] = useState();
+
+  const addD = (dNum) => {
+
+    // getDicehand()
+
+  }
+
+  const rollDice = () => {
+    // gen rando num
+    var randomNum1 = Math.ceil(Math.random() * 6);
+    // var randomNum2 = Math.floor(Math.random() * 6);
+
+    // setNewImage(diceImages[randomNum1]);
+    // setNewImage2(diceImages[randomNum2]);
+    getResult(randomNum1);
+  }
+
+  const clearDice = () => {
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Dice Roller</h1>
+
+      <div className="dicebag container">
+        <button className="button" onClick={addD(4)}>+d4</button>
+        <button className="button" onClick={addD(6)}>+d6</button>
+      </div>
+
+      <div className="dicehand container">{diceHand}</div>
+
+      <button className="button" onClick={rollDice}>ROLL</button>
+
+      <div className="result container">{result}</div>
+
+      <button className="button" onClick={clearDice}>Clear</button>
     </div>
   );
 }
